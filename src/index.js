be36b678d0f1a75a1168d60f854ddd73185ad085
hll-ai <halil.seran.1135@gmail.com>
2021-12-22 23:43:30 +0300

    WorkOn

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Card from './Card';
 import reportWebVitals from './reportWebVitals';
+import 'tachyons';
+import {robots} from './robots';
+
 
 ReactDOM.render(
-  <App/>,
-  document.getElementById('root')
+  <div>
+    <Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
+    <Card id={robots[1].id} name={robots[1].name} email={robots[1].email}/>
+    <Card id={robots[2].id} name={robots[2].name} email={robots[2].email}/>
+    <Card id={robots[3].id} name={robots[3].name} email={robots[3].email}/>2
+    <Card id={robots[4].id} name={robots[4].name} email={robots[4].email}/>4
+  </div>
+  , document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
