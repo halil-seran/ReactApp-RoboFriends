3c83fb207ea6b5ebe0f41a7602217b3e001de7e8
hll-ai <halil.seran.1135@gmail.com>
2021-12-23 23:38:18 +0300

    everythingChanged

--- src/index.js
+++ src/index.js
@@ -1,20 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Card from './Card';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
-import {robots} from './robots';
 
 
 ReactDOM.render(
-  <div>
-    <Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
-    <Card id={robots[1].id} name={robots[1].name} email={robots[1].email}/>
-    <Card id={robots[2].id} name={robots[2].name} email={robots[2].email}/>
-    <Card id={robots[3].id} name={robots[3].name} email={robots[3].email}/>2
-    <Card id={robots[4].id} name={robots[4].name} email={robots[4].email}/>4
-  </div>
+    <App/>
   , document.getElementById('root')
 );
 
