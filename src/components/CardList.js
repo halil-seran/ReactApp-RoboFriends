6f327db9864765d0b980bda0c238974662b451d3
hll-ai <halil.seran.1135@gmail.com>
2021-12-24 23:04:58 +0300

    updateV2

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -2,9 +2,9 @@ import React from "react";
 import Card from '../components/Card';
 
 const CardList = ({ robots }) => {
-    if (true) {
-        throw new Error('No!');
-    }
+    // if (true) {
+    //     throw new Error('No!');
+    // }
     return (
         <div>
             {
