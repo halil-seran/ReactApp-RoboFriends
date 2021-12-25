628e632ad5605a58dadfb66130923af668a0c821
hll-ai <halil.seran.1135@gmail.com>
2021-12-25 18:03:46 +0300

    Hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import CardList from "../components/CardList";
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
@@ -6,45 +6,56 @@ import ErrorBoundry from "../components/ErrorBoundry";
 import '../containers/App.css';
 
 
-class App extends Component {
-    constructor() {
-        super()
-        this.state = {
-            robots: [],
-            searchfield: ''
-        }
-    }
+function App() {
+    // constructor() {
+    //     super()
+    //     this.state = {
+    //         robots: [],
+    //         searchfield: ''
+    //     }
+    // }
+    const [robots, setRobots] = useState([]);
+    const [searchfield, setSearchfield] = useState('');        //setSearchfield i 27 de kullandik, searcfield = event.target.value a esitledik
+    const [count, setCount] = useState(0);
 
-    componentDidMount() {
+    // componentDidMount() {
+    //     fetch('https://jsonplaceholder.typicode.com/users')
+    //         .then(response => response.json())
+    //         .then(users => this.setState({ robots: users }));
+    // }
+
+    useEffect(() => {                                                         //useEffect runs everytime app(9) run  
         fetch('https://jsonplaceholder.typicode.com/users')
             .then(response => response.json())
-            .then(users => this.setState({ robots: users }));
+            .then(users => setRobots(users));
+    },[])                              //eger bunlardan biri degistiyse useEffecti calistir yani [] in icindekiler
+                                    //sonsuz donguye girmemesi icin bos array koyuyoruz 
+                                    //bos array degismedigi icin sadece bir kez calisiyor ama bos array koymassak [] sonsuz donguye girer
+
+    const onSearchChange = (event) => {
+        setSearchfield(event.target.value)
     }
 
-    onSearchChange = (event) => {
-        this.setState({ searchfield: event.target.value })
-    }
 
-    render() {
-        const { robots, searchfield } = this.state;
-        const filteredRobots = robots.filter(robot => {
-            return robot.name.toLowerCase().includes(searchfield.toLowerCase());
-        })
-        return !robots.length ?
-            <h1>Loading</h1>
-            : (
-                <div className="tc" >
-                    <h1 className="f1" >RoboFriends</h1>
-                    <SearchBox searchChange={this.onSearchChange} />
-                    <Scroll>
-                        <ErrorBoundry>
-                            <CardList robots={filteredRobots} />
-                        </ErrorBoundry>
-                    </Scroll>
-                </div>
-            );
-    }
+    const filteredRobots = robots.filter(robot => {
+        return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+    })
+    return !robots.length ?
+        <h1>Loading</h1>
+        : (
+            <div className="tc" >
+                <h1 className="f1" >RoboFriends</h1>
+                <button onClick={() => setCount(count+1)}>Click</button>
+                <SearchBox searchChange={onSearchChange} />
+                <Scroll>
+                    <ErrorBoundry>
+                        <CardList robots={filteredRobots} />
+                    </ErrorBoundry>
+                </Scroll>
+            </div>
+        );
 }
+
 
 
 export default App;
